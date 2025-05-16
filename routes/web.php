9d97c0829bb56f0bf4df7ba44c88f3e36d7467ab
Damon <berdiqulovolmosvek0355@gmail.com>
2025-05-16 19:56:01 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// })->middleware('auth');
-
 Route::get('/', function () {
+    return redirect('dashboard');
+})->middleware('auth');
+
+Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
